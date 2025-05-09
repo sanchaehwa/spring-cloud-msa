d5c2823b498e68436128184d871b0841785bd7c5
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-05-09 15:34:44 +0900

    [Section3] API-Gateway Service - Spring Cloud Gateway - Global Filter

--- apigateway-service/src/main/java/com/example/apigateway_service/filter/GlobalFilter.java
+++ apigateway-service/src/main/java/com/example/apigateway_service/filter/GlobalFilter.java
@@ -1,28 +1,20 @@
 package com.example.apigateway_service.filter;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.server.reactive.ServerHttpRequest; //reactive - 비동기
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
 @Slf4j
-/*
-###
 
-1. Spring Cloud Gateway : WebFlux 기반 , 비동기 + 논블로킹으로 동작함.
-2. Spring Cloud Gateway - Filter를 구현할떄, Blocking 동기 방식으로 쓰면 성능 저하
-3. Gateway - Route 목록을 봐서 매핑을 수행하는데 매핑하는 과정에서 요청, 응답을 가로챔으로써 로깅을 찍든, 인증을 수행하든, 무언가를 처리하는 역할이기때문에, Custom FIlter 를 구현할때,
-Reactive  프로그래밍 방식으로 작성.
-4. Custom Filter 구현을 위해 `AbstractGatewayFilterFactory` 를 상속받음.
- */
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
 
-public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Config> {
-
-    public CustomFilter() { //설정 클래스 등록
+    public GlobalFilter() { //설정 클래스 등록
         super(Config.class);
     }
 
@@ -38,17 +30,27 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
 
             //Filter Logging 작업
 
-            log.info("Custom PRE filter: request id -? {}",request.getId());
+            log.info("Global Filter baseMassage{}",config.getBaseMessage()); //GlobalFilter는 공통처리
+            //요청
+            if (config.isPreLogger()) {
+                log.info("Global Filter Start: request id -> {}",request.getId()); //GlobalFilter는 공통처리
+            }
 
             //Custom Post Filter
             return chain.filter(exchange).then(Mono.fromRunnable(() -> { //비동기 방식의 서버를 지원할 때, 단일값 전달할때, 모노
-                log.info("Custom POST filter: response code -> {}",response.getStatusCode());
+                //응답
+                if (config.isPostLogger()) {
+                    log.info("Global Filter End: response id -> {}",response.getStatusCode()); //GlobalFilter는 공통처리
+                }
             }));
         };
     }
 
+    @Data
     public static class Config {
         //Put the configuration properties
-
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
     }
 }
