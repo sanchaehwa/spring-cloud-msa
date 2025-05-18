7123dff62aca89a4e9decfc483ef3a7fd67d9316
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-05-18 23:29:17 +0900

    [Section5] Users Microservice Set up

---
+++ user-service/src/main/java/com/example/user_service/controller/UsersController.java
@@ -0,0 +1,28 @@
+package com.example.user_service.controller;
+
+import com.example.user_service.vo.Greeting;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user-service")
+@RequiredArgsConstructor
+public class UsersController {
+
+    private final Environment env;
+    private final Greeting greeting;
+
+    @GetMapping("/health_check")
+    public String status(){
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome(){
+       // return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+}
