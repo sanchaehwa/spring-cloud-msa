37a8d3461fecf1510063b8b63f49646424738d27
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2025-06-28 23:57:37 +0900

    [Section8] Configure Spring Cloud Gateway to use externalized settings from Spring Cloud Config

--- apigateway-service/src/main/java/com/example/apigateway_service/ApigatewayServiceApplication.java
+++ apigateway-service/src/main/java/com/example/apigateway_service/ApigatewayServiceApplication.java
@@ -1,13 +1,20 @@
 package com.example.apigateway_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayServiceApplication.class, args);
+	}
+	@Bean
+	public HttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository(); //Trace Repository 는 2.1이하 버전에서 사용
 	}
 
 }
